869c685f6348fa0683700627d4207851a7e565df
v12581234 <vinhnguyen125812@gmail.com>
2025-07-05 11:14:53 +0700

    Commit02 : feature2 Second commit

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ public class main {
         System.out.println("Hello World2");
         System.out.println("Hello World2");
         System.out.println("Hello World2");
+        System.out.println("Hello World2");
     }
 }
